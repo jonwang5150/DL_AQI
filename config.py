89548e7fb93551d09f726eb41e2588716cbd6aed
jon <124678128+jonwang5150@users.noreply.github.com>
2026-09-25 00:52:22 +0800

    change csv

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Settings:
     # 連線資訊直接寫在下一行。
     database_url = 'postgresql+psycopg://postgres:abc123@localhost:5432/postgres'
-    csv_path = 'aqi_hour_concat.csv'
+    csv_path = '20250812-20260811.csv'
     csv_encoding = 'utf-8-sig'
     batch_size = 1000
     dry_run = False
